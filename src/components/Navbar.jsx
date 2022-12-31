26de8095bc19b3be00e798dbf6156de26b1c7390
Anish Basnet <anishbasnet027@gmail.com>
2022-12-31 14:48:52 +0545

    added components layouts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,11 +26,11 @@ const Navbar = () => {
         <Menu.Item icon={<FundOutlined />}>
           <Link to="/cryptocurrencies">Cryptocurrencies</Link>
         </Menu.Item>
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
         </Menu.Item>
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/News">News</Link>
         </Menu.Item>
       </Menu>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+// import "antd/dist/antd.css";
 
 ReactDOM.render(
   <Router>
